6ea823ca8714cacc0e33d88836825b04fe7cef93
JackyLiu <jjkka132@gmail.com>
2017-07-14 13:36:51 +0800

    first commit

---
+++ Opencv_hw1and2/Opencvhw1_2/Source.cpp
@@ -0,0 +1,31 @@
+
+int HW01_TestFunction();
+int HW02_ReadAndShowData(int numberOfImgs);
+
+
+
+int main() {
+	HW01_TestFunction();
+	waitKey(0);
+	destroyAllWindows();
+
+	HW02_ReadAndShowData(8);
+
+	waitKey(0);
+	return 0;
+}
+
+
+int HW01_TestFunction() {
+
+	// Fill your answer here
+}
+
+int HW02_ReadAndShowData(int numberOfImgs) {
+	// Fill your answer here
+}
+
+
+
+
+
